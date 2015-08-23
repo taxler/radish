18ddedcb19ad730b1809534ae17f3fc00940b90a
Taxler Endiosk <taxler.endiosk@gmail.com>
2015-08-23 17:58:18 +0100

    Switch how packaging works to use an OS-based context object

--- packaging/packaging.lua
+++ packaging/packaging.lua
@@ -23,30 +23,55 @@ function package_assert(is_true, ...)
 	package_error( (...) )
 end
 
-function do_packaging(resources)
-	local icons = {}
-	do
-		local f = assert(io.open('universe/icon.(resolution @ 256,256).png', 'rb'))
-		icons[#icons+1] = {size=256, id=256, data=f:read('*a')}
-		f:close()
-	end
-	for i, icon in ipairs(icons) do
-		resources:add_icon(icon.id, icon.data)
-	end
-	resources:add_icon_group(1, icons)
+function do_packaging(in_path, out_path, context)
+	context:init(in_path)
+	context:add_app_icon('universe/icon.(resolution @ 256,256).png')
+	context:publish(out_path)
 end
 
+local context
 if ffi.os == 'Windows' then
-	local mswin = require 'exports.mswindows'
+	context = {}
+
 	local winfiles = require 'exports.mswindows.filesystem'
-	assert(winfiles.copy(in_path, temp_path, false), 'unable to copy file')
-	local resource_update = require 'exports.mswindows.handles.resource_update'
-	local resources = resource_update.begin(temp_path, true)
-	do_packaging(resources)
-	resources:commit()
-	assert(winfiles.copy(temp_path, out_path, false), 'unable to copy file')
-else
+
+	function context:init(path)
+		winfiles.copy(path, temp_path)
+		self.path = temp_path
+	end
+	context.path = temp_path
+
+
+	context.icons = {}
+	function context:add_app_icon(image_path)
+		local f = assert(io.open(image_path, 'rb'))
+		local size = assert(tonumber(image_path:match('%d+')), 'icon size not found')
+		self.icons[#self.icons+1] = {size=size, id=size, group_id=size, data=f:read('*a')}
+		f:close()
+	end
+	function context:copy_file(from_path, to_path)
+		return winfiles.copy(from_path, to_path, false)
+	end
+	
+	function context:publish(out_path)
+		local resource_update = require 'exports.mswindows.handles.resource_update'
+		local resources = resource_update.begin(temp_path, true)
+
+		for i, icon in ipairs(self.icons) do
+			resources:add_icon(icon.id, icon.data)
+			resources:add_icon_group(icon.group_id, {icon})
+		end
+		resources:add_icon_group(1, self.icons)
+
+		resources:commit()
+		winfiles.copy(self.path, out_path)
+	end
+end
+
+if context == nil then
 	error('sorry!! ' .. ffi.os .. ' platform not yet supported')
 end
+
+do_packaging(in_path, out_path, context)
 
 os.execute (string.format('start "" "%s"', out_path))
