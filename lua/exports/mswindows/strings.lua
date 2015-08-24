e805c722893caee8b98af3e0c248059c66cf5085
Taxler Endiosk <taxler.endiosk@gmail.com>
2015-08-24 13:43:15 +0100

    Use named library alias instead of ffi.C

--- lua/exports/mswindows/strings.lua
+++ lua/exports/mswindows/strings.lua
@@ -33,7 +33,7 @@ return {
 		local len = #utf8 + 1
 		local buf_size = kernel32.MultiByteToWideChar(65001, 0, utf8, len, nil, 0)
 		local buf = ffi.new('wchar_t[?]', buf_size)
-		ffi.C.MultiByteToWideChar(65001, 0, utf8, len, buf, buf_size)
+		kernel32.MultiByteToWideChar(65001, 0, utf8, len, buf, buf_size)
 		if return_size then
 			return buf, buf_size
 		end
@@ -47,7 +47,7 @@ return {
 			buf_size = buf_size - 1
 		end
 		local buf = crt.malloc(buf_size)
-		ffi.C.WideCharToMultiByte(65001, 0, ptr, len or -1, buf, buf_size, nil, nil)
+		kernel32.WideCharToMultiByte(65001, 0, ptr, len or -1, buf, buf_size, nil, nil)
 		local str = ffi.string(buf, buf_size)
 		crt.free(buf)
 		return str
