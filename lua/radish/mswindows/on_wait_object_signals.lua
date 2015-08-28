32a81c962bb6e7cfe2c437c772c4fdd7f8642714
Taxler Endiosk <taxler.endiosk@gmail.com>
2015-08-28 16:24:38 +0100

    Add method fo remove wait object signal handlers manually

--- lua/radish/mswindows/on_wait_object_signals.lua
+++ lua/radish/mswindows/on_wait_object_signals.lua
@@ -12,6 +12,42 @@ function on_wait_object_signals:add(wait_object, handler)
 	selfstate.wait_object_count = selfstate.wait_object_count + 1
 end
 
+function on_wait_object_signals:remove(wait_object, handler)
+	if type(wait_object) == 'number' then
+		local index = wait_object
+		if index < 0 or index >= selfstate.wait_object_count or index ~= math.floor(index) then
+			return false
+		end
+		for i = index, selfstate.wait_object_count - 2 do
+			selfstate.wait_objects[i] = selfstate.wait_objects[i + 1]
+		end
+		selfstate.wait_object_count = selfstate.wait_object_count - 1
+		if index == 0 then
+			on_wait_object_signals[0] = on_wait_object_signals[1]
+			if on_wait_object_signals[1] ~= nil then
+				table.remove(on_wait_object_signals, 1)
+			end
+		else
+			table.remove(on_wait_object_signals, index)
+		end
+		return true
+	end
+	if type(wait_object) == 'function' then
+		local handler = wait_object
+		for i = 0, #on_wait_object_signals do
+			if handler == on_wait_object_signals[i] then
+				return self:remove(i)
+			end
+		end
+		return
+	end
+	for i = 0, #on_wait_object_signals do
+		if handler == on_wait_object_signals[i] and wait_object == selfstate.wait_objects[i] then
+			return self:remove(i)
+		end
+	end
+end
+
 local on_other_events = require 'radish.mswindows.on_other_events'
 
 on_other_events[selflib.WMRADISH_WAIT_OBJECT_SIGNALLED] = function(_, message, wparam, lparam)
@@ -21,18 +57,7 @@ on_other_events[selflib.WMRADISH_WAIT_OBJECT_SIGNALLED] = function(_, message, w
 		result = handler(ffi.cast('void*', wparam))
 	end
 	if result == 'remove' then
-		for i = lparam, wait_object_count - 2 do
-			selfstate.wait_objects[i] = selfstate.wait_objects[i + 1]
-		end
-		selfstate.wait_object_count = selfstate.wait_object_count - 1
-		if lparam == 0 then
-			on_wait_object_signals[0] = on_wait_object_signals[1]
-			if on_wait_object_signals[1] ~= nil then
-				table.remove(on_wait_object_signals, 1)
-			end
-		else
-			table.remove(on_wait_object_signals, lparam)
-		end
+		on_wait_object_signals:remove(lparam)
 	end
 end
 
