25b26561998bea3a2bc567c7325fe852fd697255
Taxler Endiosk <taxler.endiosk@gmail.com>
2015-09-10 23:03:45 +0100

    Clarify status of serialization, check table cycles and allow for pointer transmission

--- lua/radish/mswindows/task/comms.lua
+++ lua/radish/mswindows/task/comms.lua
@@ -1,11 +1,30 @@
 
+-- NOTE: the serialization here is ONLY intended for communication between threads
+--  in a running process, NOT for saving to disk...!!
+
 local ffi = require 'ffi'
 
 local comms = {}
 
 local tostring_types = {number=true, boolean=true, ['nil']=true}
 
-local function append_value(buf, value)
+local ptr_meta = {
+	__serialize = function(self, buf)
+		local ptr_buf = ffi.new('void*[1]', self.ptr)
+		local stringed = ffi.string(ptr_buf, ffi.sizeof 'void*')
+		if self.lib then
+			buf[#buf+1] = string.format('ptr(%q, %q, %q)', stringed, self.type, self.lib)
+		else
+			buf[#buf+1] = string.format('ptr(%q, %q)', stringed, self.type)
+		end
+	end;
+}
+
+function comms.ptr(ptr, type, lib)
+	return setmetatable({ptr=ptr, type=type, lib=lib}, ptr_meta)
+end
+
+local function append_value(buf, value, tables)
 	local vtype = type(value)
 	if vtype == 'string' then
 		buf[#buf+1] = string.format('%q', value)
@@ -16,20 +35,28 @@ local function append_value(buf, value)
 		buf[#buf+1] = tostring(value)
 		return
 	end
-	-- TODO: check for & error on table cycles
+	local m = getmetatable(value)
+	if type(m) == 'table' and m.__serialize then
+		m.__serialize(value, buf)
+		return
+	end
 	if vtype == 'table' then
+		if tables[value] then
+			error('cannot serialize table with cycles')
+		end
+		tables[value] = true
 		buf[#buf+1] = '{'
 		for k, v in pairs(value) do
 			buf[#buf+1] = '['
-			append_value(buf, k)
+			append_value(buf, k, tables)
 			buf[#buf+1] = ']='
-			append_value(buf, v)
+			append_value(buf, v, tables)
 			buf[#buf+1] = ';'
 		end
 		buf[#buf+1] = '}'
+		tables[value] = nil
 		return
 	end
-	-- TODO: support cdata pointers
 	error('unable to serialize:' .. tostring(value))
 end
 
@@ -39,13 +66,20 @@ function comms.serialize(...)
 		if i > 1 then
 			buf[#buf+1] = ', '
 		end
-		append_value(buf, select(i, ...))
+		append_value(buf, select(i, ...), {})
 	end
 	return table.concat(buf)
 end
 
+local function ptr(stringed, type, lib)
+	if lib then
+		require(lib)
+	end
+	return ffi.cast(type, ffi.cast('void**', stringed)[0])
+end
+
 function comms.deserialize(serialized)
-	return assert(loadstring('return ' .. serialized))()
+	return assert(loadstring('local ptr=...;return ' .. serialized))(ptr)
 end
 
 return comms
