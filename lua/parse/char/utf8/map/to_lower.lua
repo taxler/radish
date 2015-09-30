24b8ff325cc1baff54e353fe56d3447cc89af12f
Taxler Endiosk <taxler.endiosk@gmail.com>
2015-09-30 17:40:37 +0100

    More case-mapping fixes

--- lua/parse/char/utf8/map/to_lower.lua
+++ lua/parse/char/utf8/map/to_lower.lua
@@ -1,5 +1,5 @@
 return require('parse.char.utf8.map.to_upper.reversible'):reverse() + {
-	'İi';'ǅǆ';'ǈǉ';'ǋǌ';'ǲǳ';'ϴθ';'ẞß';'Ωω';'Kk';'Åå';'İi̇';
+	'İi';'ǅǆ';'ǈǉ';'ǋǌ';'ǲǳ';'ϴθ';'ẞß';'Ωω';'Kk';'Åå';
 	{
 		'\u{3a3}\u{3c2}';
 		check_suffix = require 'parse.char.utf8.peek.Final_Sigma';
